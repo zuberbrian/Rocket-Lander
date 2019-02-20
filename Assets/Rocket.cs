ed5c613a1da5ed8beffa87701c291d1a6e28bc31
zuberbrian <37417090+zuberbrian@users.noreply.github.com>
2019-02-19 20:27:23 -0800

    reworked thrusters

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 100f;
 	Rigidbody rigidBody;
 	AudioSource audioSource;
 
@@ -16,25 +18,39 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ProcessInput();
+		Thrust();
+		Rotate();
 	}
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space)){
-			rigidBody.AddRelativeForce(Vector3.up);
-			if (!audioSource.isPlaying){			
-				audioSource.Play();
-			}
-		}
-		else{
-			audioSource.Stop();
-		}
-        if (Input.GetKey(KeyCode.A)){
-			transform.Rotate(Vector3.forward);
-		}
-		else if (Input.GetKey(KeyCode.D)){
-			transform.Rotate(-Vector3.forward);
-		}
+		rigidBody.freezeRotation = true; // manual control of rotation
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+		rigidBody.freezeRotation = false; // end of manual rotation control
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
